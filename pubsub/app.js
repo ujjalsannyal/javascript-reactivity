7e1a7467c14eb24b3e3968f4fa8583534734a327
ujjal sannyal <ujjal.sannyal10@gmail.com>
2024-12-05 16:37:51 +0530

    added coment

--- pubsub/app.js
+++ pubsub/app.js
@@ -12,14 +12,15 @@ pubSubInstance.subscribe(payload =>  {
     console.log('subscribe 3: show message => ', payload)
 })
 
-// pubSubInstance.subscribe(console.log('subscribe 3: show message => '))
+// pubSubInstance.subscribe(console.log('subscribe 3: show message => ')) // It will trigger error
 
 pubSubInstance.publish('hello world')
 
-sub2.unsubscribe();
+sub2.unsubscribe(); // Subscription 2 should not render any further publilsh data
 
 pubSubInstance.publish('hello world 2')
 
+// All three subscription are removed.
 pubSubInstance.unsubscribeAll();
 
 pubSubInstance.publish('After removing all subscription');
